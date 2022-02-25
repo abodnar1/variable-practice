7d76365675ff84f29bbedc41ec2bf6f2e78427c5
Amber Bodnar <amber.bodnar@gmail.com>
2022-02-24 22:40:59 -0500

    Add string annotations

--- variables.js
+++ variables.js
@@ -1,6 +1,14 @@
+
+//the variable catName is defined and assigned "Astig"
 var catName = "Astig";
+
+//the variable kittenName is defined and assigned "Kumquat"
 var kittenName = "Kumquat";
+
+//the variable girlfriendName is defined and assigned "Nicole"
 var girlfriendName = "Nicole";
+
+//the variable liveInState is defined and assigned "Georgia"
 var liveInState = "Georgia";
 
 
